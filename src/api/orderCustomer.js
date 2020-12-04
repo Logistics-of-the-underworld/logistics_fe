d883a156f512980e9abb907c54d912aaf106bf67
wonichan <1012403910@qq.com>
2020-12-04 21:35:51 +0800

    订单管理+用户订单查询

---
+++ src/api/orderCustomer.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function fetchList(query) {
+  return request({
+    url: '/orderUser/getAllOrder',
+    method: 'post',
+    params: query
+  })
+}
+
+export function createOrder(params) {
+  return request({
+    url: '/orderUser/createOrder',
+    method: 'post',
+    params: params
+  })
+}
+
+export function deleteOrder(params) {
+  return request({
+    url: `/orderUser/deleteOrder/${params}`,
+    method: 'get'
+  })
+}
+
+export function updateOrder(params) {
+  return request({
+    url: '/orderUser/updateOrder',
+    method: 'post',
+    params: params
+  })
+}
--- src/views/order/order_customer/orderCustomerPage.vue
+++ src/views/order/order_customer/orderCustomerPage.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.title" placeholder="收寄地" style="width: 200px;margin-right:12px" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.receiverAddress" placeholder="收寄地" style="width: 200px;margin-right:12px" class="filter-item" @keyup.enter.native="handleFilter" />
       <el-select v-model="listQuery.importance" placeholder="用户评价" clearable style="width: 120px;margin-right:12px" class="filter-item">
         <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
       </el-select>
-      <el-select v-model="listQuery.type" placeholder="订单状态" clearable class="filter-item" style="width: 130px;margin-right:12px">
-        <el-option v-for="item in orderStateTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
-      </el-select>
-      <el-select v-model="listQuery.sort" style="width: 140px;margin-right:12px" class="filter-item" @change="handleFilter">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
+      <el-select v-model="listQuery.stateOrder" placeholder="订单状态" clearable class="filter-item" style="width: 130px;margin-right:12px">
+        <el-option v-for="item in orderStateOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
       </el-select>
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         查询
@@ -35,54 +32,54 @@
         style="width: 100%"
         @sort-change="sortChange"
       >
-        <el-table-column label="订单编号" prop="id_order" sortable="custom" align="center" width="180" :class-name="getSortClass('id')">
+        <el-table-column label="订单编号" prop="idOrder" sortable="custom" align="center" :class-name="getSortClass('id')">
           <template slot-scope="{row}">
-            <span>{{ row.id_order }}</span>
+            <span>{{ row.idOrder }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="下单时间" width="150px" align="center">
+        <el-table-column label="下单时间" align="center">
           <template slot-scope="{row}">
-            <span>{{ row.create_time | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+            <span>{{ row.createTime | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="配送点名称" width="150px" align="center">
+        <el-table-column label="配送点名称" align="center">
           <template slot-scope="{row}">
-            <span>{{ row.name_distribution }}</span>
+            <span>{{ row.nameDistribution }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="寄件人姓名" width="110px" align="center">
+        <el-table-column label="寄件人姓名" align="center">
           <template slot-scope="{row}">
-            <span>{{ row.sender_name }}</span>
+            <span>{{ row.senderName }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="收件人姓名" width="110px" align="center">
+        <el-table-column label="收件人姓名" align="center">
           <template slot-scope="{row}">
-            <span>{{ row.receiver_name }}</span>
+            <span>{{ row.receiverName }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="配送价格" width="110px" align="center">
+        <el-table-column label="配送价格" align="center">
           <template slot-scope="{row}">
-            <span>{{ row.delivery_price }}</span>
+            <span>{{ row.deliveryPrice }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="评价" width="110px">
+        <el-table-column label="评价">
           <template slot-scope="{row}">
             <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="meta-item__icon" />
           </template>
         </el-table-column>
-        <el-table-column label="订单状态" align="center" width="95">
+        <el-table-column label="订单状态" align="center">
           <template slot-scope="{row}">
-            <el-tag :type="row.state_order | statusFilter">
-              {{ row.state_order }}
+            <el-tag :type="row.stateOrder | statusFilter">
+              {{ orderStateTypeKeyValue[row.stateOrder] }}
             </el-tag>
           </template>
         </el-table-column>
-        <el-table-column v-if="showReviewer" label="Reviewer" width="110px" align="center">
+        <el-table-column v-if="showReviewer" label="Reviewer" align="center">
           <template slot-scope="{row}">
             <span style="color:red;">{{ row.reviewer }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="操作" align="center" width="200" class-name="small-padding fixed-width">
+        <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
           <template slot-scope="{row,$index}">
             <el-button type="primary" size="mini" @click="handleUpdate(row)">
               查看
@@ -96,46 +93,51 @@
     </div>
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
 
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="订单编号" prop="id_order">
-          <el-input v-model="temp.id_order" />
+    <el-dialog title="更新订单信息" :visible.sync="dialogFormVisible">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="180px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="订单编号" prop="idOrder">
+          <el-input v-model="temp.idOrder" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="下单时间" prop="create_time">
-          <el-date-picker v-model="temp.create_time" type="datetime" placeholder="Please pick a date" disabled />
+        <el-form-item label="下单时间" prop="createTime">
+          <el-date-picker v-model="temp.createTime" type="datetime" placeholder="Please pick a date" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="配送时间" prop="delivery_time">
-          <el-input v-model="temp.delivery_time" />
+        <el-form-item label="配送时间" prop="deliveryTime">
+          <el-input v-model="temp.deliveryTime" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="配送点名称" prop="name_distribution">
-          <el-input v-model="temp.name_distribution" />
+        <el-form-item label="配送点名称" prop="nameDistribution">
+          <el-input v-model="temp.nameDistribution" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="条形码" prop="bar_code_url">
-          <el-input v-model="temp.bar_code_url" />
+        <el-form-item label="条形码" prop="barCodeUrl">
+          <el-input v-model="temp.barCodeUrl" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="寄件人姓名" prop="sender_name">
-          <el-input v-model="temp.sender_name" disabled />
+        <el-form-item label="配送车辆" prop="idLicense">
+          <el-input v-model="temp.idLicense" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="寄件人电话" prop="sender_phone">
-          <el-input v-model="temp.sender_phone" disabled />
+        <el-form-item label="寄件人姓名" prop="senderName">
+          <el-input v-model="temp.senderName" />
         </el-form-item>
-        <el-form-item label="收件人姓名" prop="receiver_name">
-          <el-input v-model="temp.receiver_name" />
+        <el-form-item label="寄件人电话" prop="senderPhone">
+          <el-input v-model="temp.senderPhone" />
         </el-form-item>
-        <el-form-item label="收件人电话" prop="receiver_phone">
-          <el-input v-model="temp.receiver_phone" />
+        <el-form-item label="收件人姓名" prop="receiverName">
+          <el-input v-model="temp.receiverName" />
         </el-form-item>
-        <el-form-item label="配送车辆" prop="id_license">
-          <el-input v-model="temp.id_license" />
+        <el-form-item label="收件人电话" prop="receiverPhone">
+          <el-input v-model="temp.receiverPhone" />
         </el-form-item>
-        <el-form-item label="订单状态" prop="state_order">
-          <el-input v-model="temp.state_order" />
+        <el-form-item label="订单状态" prop="stateOrder">
+          <el-select v-model="temp.stateOrder" class="filter-item" placeholder="Please select" :disabled="'update' === dialogStatus">
+            <el-option v-for="item in orderStateOptions" :key="item.display_name" :label="item.display_name" :value="item.key" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="支付方式" prop="paymentMethod">
+          <el-input v-model="temp.paymentMethod" :disabled="'update' === dialogStatus" />
         </el-form-item>
         <el-form-item label="配送人" prop="courier">
-          <el-input v-model="temp.courier" />
+          <el-input v-model="temp.courier" :disabled="'update' === dialogStatus" />
         </el-form-item>
-        <el-form-item label="配送人电话" prop="courier_phone">
-          <el-input v-model="temp.courier_phone" />
+        <el-form-item label="配送人电话" prop="courierPhone">
+          <el-input v-model="temp.courierPhone" :disabled="'update' === dialogStatus" />
         </el-form-item>
         <el-form-item label="用户评价">
           <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />
@@ -154,13 +156,14 @@
       </div>
     </el-dialog>
 
-    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
-      <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
-        <el-table-column prop="key" label="Channel" />
-        <el-table-column prop="pv" label="Pv" />
-      </el-table>
+    <el-dialog :visible.sync="creatOrderVisible" title="新建订单">
+      <el-form ref="orderForm" :rules="rules" :model="pvData" label-position="left" label-width="180px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="寄件人姓名" prop="senderName">
+          <el-input v-model="pvData.senderName" />
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
+        <el-button type="primary" @click="creatOrderVisible = false">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
@@ -171,21 +174,9 @@ import { fetchPv } from '@/api/article'
 import waves from '@/directive/waves' // waves directive
 import { parseTime } from '@/utils'
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
-import { fetchList, createOrder, updateOrder, deleteOrder } from '@/api/order'
-const orderTest = { // eslint-disable-line no-unused-vars
-  items: [
-    {
-      id_order: 'zxc123456', name_distribution: '灵力药房', bar_code_url: '/asdkjhajkfasf.jpg', state_order: 6,
-      sender_name: '我急了', sender_phone: 15023421665, receiver_name: '我装的', receiver_phone: '110',
-      delivery_price: '18.00', insurance_fee: '0.00', delivery_time: '2020-11-27 16:04', id_license: '渝AE86',
-      courier: '陈快递', courier_phone: '12580', marks: '这里是备注', create_time: '1998-08-01 07:42', importance: 3,
-      reviewer: '熊洪伟'
-    }
-  ],
-  total: 1
-}
+import { fetchList, createOrder, updateOrder, deleteOrder } from '@/api/orderCustomer'
 
-const orderStateTypeOptions = [
+const orderStateOptions = [
   { key: 0, display_name: '未处理' },
   { key: 1, display_name: '审核通过' },
   { key: 2, display_name: '在途中' },
@@ -195,7 +186,7 @@ const orderStateTypeOptions = [
   { key: 6, display_name: '已完成' },
   { key: 7, display_name: '退订' }
 ]
-const orderStateTypeKeyValue = orderStateTypeOptions.reduce((acc, cur) => {
+const orderStateTypeKeyValue = orderStateOptions.reduce((acc, cur) => {
   acc[cur.key] = cur.display_name
   return acc
 }, {})
@@ -221,53 +212,61 @@ export default {
   data() {
     return {
       tableKey: 0,
-      list: null,
+      list: undefined,
       total: 0,
       listLoading: true,
-      orderStateTypeOptions,
+      orderStateOptions,
+      orderStateTypeKeyValue,
       listQuery: {
         page: 1,
         limit: 20,
         importance: undefined,
-        title: undefined,
-        type: undefined,
-        sort: '+id'
+        receiverAddress: undefined,
+        stateOrder: undefined
       },
       importanceOptions: [1, 2, 3],
       sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
-      statusOptions: ['published', 'draft', 'deleted'],
       showReviewer: false,
       temp: {
-        id_order: undefined,
-        name_distribution: '',
-        bar_code_url: undefined,
+        idOrder: undefined,
+        nameDistribution: '',
+        barCodeUrl: undefined,
         importance: 1,
-        sender_name: '',
-        sender_phone: undefined,
-        receiver_name: '',
-        receiver_phone: undefined,
-        create_time: undefined,
-        timestamp: new Date(),
-        delivery_time: undefined,
-        id_license: undefined,
-        state_order: undefined,
+        senderName: '',
+        senderPhone: undefined,
+        receiverName: '',
+        receiverPhone: undefined,
+        createTime: undefined,
+        deliveryTime: undefined,
+        idLicense: undefined,
+        stateOrder: undefined,
         courier: '',
-        courier_phone: undefined,
+        courierPhone: undefined,
         marks: ''
       },
       dialogFormVisible: false,
       dialogStatus: '',
       textMap: {
-        update: 'Edit',
-        create: 'Create'
+        update: '更新订单信息',
+        create: '创建订单信息'
       },
-      dialogPvVisible: false,
-      pvData: [],
+      creatOrderVisible: false,
+      pvData: {
+        idOrder: undefined,
+        senderName: '',
+        senderPhone: undefined,
+        receiverName: '',
+        receiverPhone: undefined,
+        paymentMethod: undefined,
+        createTime: undefined,
+        courier: '',
+        courierPhone: undefined,
+        marks: ''
+      },
       rules: {
-        id_license: [{ required: true, message: 'id_license is required', trigger: 'change' }],
-        timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
-        receiver_name: [{ required: true, message: 'receiver_name is required', trigger: 'blur' }],
-        receiver_phone: [{ required: true, message: 'receiver_phone is required', trigger: 'blur' }]
+        idLicense: [{ required: true, message: 'idLicense is required', trigger: 'change' }],
+        receiverName: [{ required: true, message: 'receiverName is required', trigger: 'blur' }],
+        receiverPhone: [{ required: true, message: 'receiverPhone is required', trigger: 'blur' }]
       },
       downloadLoading: false
     }
@@ -277,16 +276,14 @@ export default {
   },
   methods: {
     async getList() {
-      console.log(this.listQuery)
       this.listLoading = true
       await fetchList(this.listQuery).then(res => {
-        const items = res.data.items
-        this.total = res.data.total
-        this.list = items.map(v => {
-          v.state_order = orderStateTypeKeyValue[v.state_order]
+        this.total = res.total
+        this.list = res.data
+        console.log(this.list)
+        this.list = this.list.map(v => {
+          v.createTime = this.dayjs(v.createTime).format('YYYY-MM-DD HH:mm')
           return v
-        }).catch(err => {
-          console.log(err)
         })
         this.listLoading = false
         console.log(this.listQuery.importance)
@@ -319,40 +316,51 @@ export default {
     },
     resetTemp() {
       this.temp = {
-        id_order: undefined,
-        name_distribution: '',
-        bar_code_url: undefined,
+        idOrder: undefined,
+        nameDistribution: '',
+        barCodeUrl: undefined,
         importance: 1,
-        sender_name: '',
-        sender_phone: undefined,
-        receiver_name: '',
-        receiver_phone: undefined,
-        create_time: undefined,
-        timestamp: new Date(),
-        delivery_time: undefined,
-        id_license: undefined,
-        state_order: undefined,
+        senderName: '',
+        senderPhone: undefined,
+        receiverName: '',
+        receiverPhone: undefined,
+        createTime: undefined,
+        deliveryTime: undefined,
+        idLicense: undefined,
+        stateOrder: undefined,
         courier: '',
-        courier_phone: undefined,
+        courierPhone: undefined,
+        marks: ''
+      }
+    },
+    restOderTemp() {
+      this.pvData = {
+        idOrder: undefined,
+        senderName: '',
+        senderPhone: undefined,
+        receiverName: '',
+        receiverPhone: undefined,
+        paymentMethod: undefined,
+        createTime: undefined,
+        courier: '',
+        courierPhone: undefined,
         marks: ''
       }
     },
     handleCreate() {
-      this.resetTemp()
-      this.dialogStatus = 'create'
-      this.dialogFormVisible = true
+      this.restOderTemp()
+      this.creatOrderVisible = true
       this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
+        this.$refs['orderForm'].clearValidate()
       })
     },
     createData() {
-      this.$refs['dataForm'].validate((valid) => {
+      this.$refs['orderForm'].validate((valid) => {
         if (valid) {
-          this.temp.id = parseInt(Math.random() * 100) + 1024 // mock a id
-          this.temp.author = 'vue-element-admin'
-          createOrder(this.temp).then(() => {
-            this.list.unshift(this.temp)
-            this.dialogFormVisible = false
+          this.pvData.idOrder = parseInt(Math.random() * 100) + 1024 // mock a id
+          createOrder({ order: this.pvData }).then(() => {
+            this.list.unshift(this.pvData)
+            this.creatOrderVisible = false
             this.$notify({
               title: 'Success',
               message: 'Created Successfully',
@@ -365,7 +373,6 @@ export default {
     },
     handleUpdate(row) {
       this.temp = Object.assign({}, row) // copy obj
-      this.temp.timestamp = new Date(this.temp.timestamp)
       this.dialogStatus = 'update'
       this.dialogFormVisible = true
       this.$nextTick(() => {
@@ -376,8 +383,7 @@ export default {
       this.$refs['dataForm'].validate((valid) => {
         if (valid) {
           const tempData = Object.assign({}, this.temp)
-          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
-          updateOrder(tempData).then(() => {
+          updateOrder({ order: tempData }).then(() => {
             const index = this.list.findIndex(v => v.id === this.temp.id)
             this.list.splice(index, 1, this.temp)
             this.dialogFormVisible = false
@@ -405,12 +411,12 @@ export default {
       })
     },
     deleteData(row) {
-      return deleteOrder(row)
+      return deleteOrder(row.idOrder)
     },
     handleFetchPv(pv) {
       fetchPv(pv).then(response => {
         this.pvData = response.data.pvData
-        this.dialogPvVisible = true
+        this.creatOrderVisible = true
       })
     },
     handleDownload() {
