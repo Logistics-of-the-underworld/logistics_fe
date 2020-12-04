58173b11268dda26d9d1499a8f54c603f18758dd
YueWu <17184030601@163.com>
2020-12-04 09:40:24 +0800

    修改登出功能报错

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -91,21 +91,22 @@ const actions = {
   // user logout
   logout({ commit, state, dispatch }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '')
-        commit('SET_ROLES', [])
-        commit('SET_NAME', '')
-        removeToken()
-        resetRouter()
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      commit('SET_NAME', '')
+      commit('SET_ICON', '')
+      commit('SET_PHONE', '')
+      commit('SET_PETNAME', '')
+      commit('SET_EMAIL', '')
+      commit('SET_ORGANIZATIONNAME', '')
+      removeToken()
+      resetRouter()
 
-        // reset visited views and cached views
-        // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
-        dispatch('tagsView/delAllViews', null, { root: true })
+      // reset visited views and cached views
+      // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
+      dispatch('tagsView/delAllViews', null, { root: true })
 
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
+      resolve()
     })
   },
 
